092e934ec46366fa971e75105d7426635c743a94
Romchec <60809359+Romchec@users.noreply.github.com>
2022-06-20 02:25:28 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo cp -rv strawberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.strawberry/
+sudo cp -rv orange/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.orange/
+sudo cp -rv banana/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.banana/
+sudo cp -rv lime/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.lime/
+sudo cp -rv mint/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.mint/
+sudo cp -rv blueberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.blueberry/
+sudo cp -rv grape/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.grape/
+sudo cp -rv bubblegum/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.bubblegum/
+sudo cp -rv cocoa/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.cocoa/
+sudo cp -rv slate/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.slate/
