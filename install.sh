19ec7557467d9e36891fa85a582473533f179eca
Romchec <roman.vyach.ivanov@gmail.com>
2022-06-30 23:24:22 +0500

    bash -> sh, removed sudo

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo cp -rv strawberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.strawberry/
-sudo cp -rv orange/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.orange/
-sudo cp -rv banana/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.banana/
-sudo cp -rv lime/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.lime/
-sudo cp -rv mint/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.mint/
-sudo cp -rv blueberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.blueberry/
-sudo cp -rv grape/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.grape/
-sudo cp -rv bubblegum/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.bubblegum/
-sudo cp -rv cocoa/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.cocoa/
-sudo cp -rv slate/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.slate/
+cp -rv strawberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.strawberry/
+cp -rv orange/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.orange/
+cp -rv banana/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.banana/
+cp -rv lime/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.lime/
+cp -rv mint/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.mint/
+cp -rv blueberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.blueberry/
+cp -rv grape/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.grape/
+cp -rv bubblegum/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.bubblegum/
+cp -rv cocoa/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.cocoa/
+cp -rv slate/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.slate/
