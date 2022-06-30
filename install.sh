6438e84fdffc10d907cd2dcecaf575a4736cfd4a
Romchec <roman.vyach.ivanov@gmail.com>
2022-07-01 00:02:58 +0500

    Minor

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-cp -rv strawberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.strawberry/
-cp -rv orange/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.orange/
-cp -rv banana/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.banana/
-cp -rv lime/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.lime/
-cp -rv mint/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.mint/
-cp -rv blueberry/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.blueberry/
-cp -rv grape/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.grape/
-cp -rv bubblegum/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.bubblegum/
-cp -rv cocoa/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.cocoa/
-cp -rv slate/gtk-2.0/ /usr/share/themes/io.elementary.stylesheet.slate/
+dir='/usr/share/themes/io.elementary.stylesheet'
+
+cp  -rv  'strawberry/gtk-2.0/'  "$dir.strawberry/"
+cp  -rv  'orange/gtk-2.0/'      "$dir.orange/"
+cp  -rv  'banana/gtk-2.0/'      "$dir.banana/"
+cp  -rv  'lime/gtk-2.0/'        "$dir.lime/"
+cp  -rv  'mint/gtk-2.0/'        "$dir.mint/"
+cp  -rv  'blueberry/gtk-2.0/'   "$dir.blueberry/"
+cp  -rv  'grape/gtk-2.0/'       "$dir.grape/"
+cp  -rv  'bubblegum/gtk-2.0/'   "$dir.bubblegum/"
+cp  -rv  'cocoa/gtk-2.0/'       "$dir.cocoa/"
+cp  -rv  'slate/gtk-2.0/'       "$dir.slate/"
